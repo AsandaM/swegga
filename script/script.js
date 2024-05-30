ca79d4796a39ab2836542f5875a5a0aff626227c
AsandaM <asandamehlo088@gmail.com>
2024-05-30 15:41:26 +0200

    edited

--- script/script.js
+++ script/script.js
@@ -11,15 +11,35 @@ let total = document.querySelectorAll('td')[14]
 
 let totalAll
 
-function calc() {
-    totalFemale.innerText = (quantityFemale.value * priceFemale.innerText).toFixed(2)
-    totalMale.innerText = (quantityMale.value * priceMale.innerText).toFixed(2)
-    totalAll = eval(`${totalFemale.innerText} + ${totalMale.innerText}`)
+function calc(fQuantity, mQuantity) {
+    // totalFemale.innerText = (quantityFemale.value * priceFemale.innerText).toFixed(2)
+    // totalMale.innerText = (quantityMale.value * priceMale.innerText).toFixed(2)
+    let arr = [totalFemale.innerText, totalMale.innerText].join().split('R').join().split(',').reduce((total,current)=>{
+        return total + +current
+    },0)
+    console.log(arr);
+    // totalAll = eval(`${totalFemale.innerText} + ${totalMale.innerText}`)
+    // totalAll = eval(`${fQuantity} + ${mQuantity}`)
 
-    
-
-    total.innerText = totalAll.toFixed(2)
+    // total.innerText = 'R' + totalAll.toFixed(2)
+    total.innerText = 'R' + arr.toFixed(2)
     
 }
+buttonTotal.addEventListener('click', ()=>{
+    // let fQuantity = quantityFemale.value * priceFemale.innerText
+    // let mQuantity = quantityMale.value * priceMale.innerText
+    // calc(fQuantity,mQuantity)
+    calc()
+})
 
-buttonTotal.addEventListener('click', calc)
+function FemaleTotal() {
+    totalFemale.innerText = 'R ' + (quantityFemale.value * priceFemale.innerText).toFixed(2)
+}
+function MaleTotal() {
+    totalMale.innerText = 'R ' + (quantityMale.value * priceMale.innerText).toFixed(2)
+}
+
+quantityFemale.onchange = function () {FemaleTotal()}
+quantityMale.onchange = function () {MaleTotal()}
+
+// buttonTotal.onmouseover = function(){calc()}
