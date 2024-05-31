95fe9d68a3550d271d538e8709afa8f3fb6cf852
AsandaM <asandamehlo088@gmail.com>
2024-05-31 08:48:12 +0200

    Edited

--- script/script.js
+++ script/script.js
@@ -9,26 +9,18 @@ let totalFemale = document.querySelectorAll('td')[5]
 let totalMale = document.querySelectorAll('td')[11]
 let total = document.querySelectorAll('td')[14]
 
-let totalAll
+// let totalAll
 
 function calc(fQuantity, mQuantity) {
-    // totalFemale.innerText = (quantityFemale.value * priceFemale.innerText).toFixed(2)
-    // totalMale.innerText = (quantityMale.value * priceMale.innerText).toFixed(2)
+    
     let arr = [totalFemale.innerText, totalMale.innerText].join().split('R').join().split(',').reduce((total,current)=>{
         return total + +current
     },0)
-    console.log(arr);
-    // totalAll = eval(`${totalFemale.innerText} + ${totalMale.innerText}`)
-    // totalAll = eval(`${fQuantity} + ${mQuantity}`)
 
-    // total.innerText = 'R' + totalAll.toFixed(2)
     total.innerText = 'R' + arr.toFixed(2)
     
 }
 buttonTotal.addEventListener('click', ()=>{
-    // let fQuantity = quantityFemale.value * priceFemale.innerText
-    // let mQuantity = quantityMale.value * priceMale.innerText
-    // calc(fQuantity,mQuantity)
     calc()
 })
 
